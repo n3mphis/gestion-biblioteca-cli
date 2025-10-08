5a4086b8aeb392e0af42085a3bc98cd0cee8baaa
n3mphis <66238416+n3mphis@users.noreply.github.com>
2025-10-08 14:46:06 -0300

    Creada clase Prestamo para definiar que libro y que usuario pidio un prestamo del libro

---
+++ src/main/java/com/mibiblioteca/gestion_biblioteca_cli/model/Prestamo.java
@@ -0,0 +1,41 @@
+package com.mibiblioteca.gestion_biblioteca_cli.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "prestamos")
+@Getter
+@NoArgsConstructor
+public class Prestamo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate fechaPrestamo;
+    private LocalDate fechaDevolucionEstimada;
+
+    @Setter
+    private LocalDate fechaDevolucionReal;
+
+    @ManyToOne
+    private Libro libro;
+
+    @ManyToOne
+    private Usuario usuario;
+
+    public Prestamo(Libro libro, Usuario usuario, LocalDate fechaDevolucionEstimada) {
+        this.libro = libro;
+        this.usuario = usuario;
+        this.fechaDevolucionEstimada = fechaDevolucionEstimada;
+
+        this.fechaPrestamo = LocalDate.now();
+        this.fechaDevolucionReal = null;
+    }
+}
