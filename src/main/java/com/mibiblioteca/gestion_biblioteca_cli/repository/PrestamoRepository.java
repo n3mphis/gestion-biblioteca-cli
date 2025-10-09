cf6b888937894d874566be4cb8fd386c4bcab71e
n3mphis <66238416+n3mphis@users.noreply.github.com>
2025-10-09 18:50:02 -0300

    Agregados metodos (usando JPQL) para querys más precisas y con estadísticas

--- src/main/java/com/mibiblioteca/gestion_biblioteca_cli/repository/PrestamoRepository.java
+++ src/main/java/com/mibiblioteca/gestion_biblioteca_cli/repository/PrestamoRepository.java
@@ -1,7 +1,10 @@
 package com.mibiblioteca.gestion_biblioteca_cli.repository;
 
+import com.mibiblioteca.gestion_biblioteca_cli.model.Libro;
 import com.mibiblioteca.gestion_biblioteca_cli.model.Prestamo;
+import com.mibiblioteca.gestion_biblioteca_cli.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -13,4 +16,13 @@ public interface PrestamoRepository extends JpaRepository<Prestamo, Long> {
 
     List<Prestamo> findByFechaDevolucionRealIsNull();
     List<Prestamo> findByFechaPrestamoBetween(LocalDate fechaInicio, LocalDate fechaFin);
+
+    @Query("SELECT p FROM Prestamo p WHERE p.fechaDevolucionReal IS NULL AND p.fechaDevolucionEstimada < CURRENT_DATE")
+    List<Prestamo> findPrestamosVencidos();
+
+    @Query("SELECT p.libro FROM Prestamo p GROUP BY p.libro ORDER BY COUNT(p) DESC LIMIT 1")
+    Optional<Libro> findLibroMasPrestado();
+
+    @Query("SELECT p.usuario FROM Prestamo p GROUP BY p.usuario ORDER BY COUNT(p) DESC LIMIT 1")
+    Optional<Usuario> findUsuariosConMasPrestamos();
 }
