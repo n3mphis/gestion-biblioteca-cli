9b7d8781a84e727d838273213dcbf6b57da8e555
n3mphis <66238416+n3mphis@users.noreply.github.com>
2025-10-10 13:35:10 -0300

    Creada nueva excepcion

---
+++ src/main/java/com/mibiblioteca/gestion_biblioteca_cli/exceptions/FormatoDniInvalidoException.java
@@ -0,0 +1,7 @@
+package com.mibiblioteca.gestion_biblioteca_cli.exceptions;
+
+public class FormatoDniInvalidoException extends RuntimeException {
+    public FormatoDniInvalidoException(String message) {
+        super(message);
+    }
+}
